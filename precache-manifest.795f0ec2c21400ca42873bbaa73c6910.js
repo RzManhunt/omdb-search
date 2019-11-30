6833456ada58dba0f5ace9e76e3af3d08fc26993
Luis <lamsarmiento@gmail.com>
2019-11-30 19:59:58 -0400

    Updates

---
+++ precache-manifest.795f0ec2c21400ca42873bbaa73c6910.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e776e3a7595672f7082102139f7c69f1",
+    "url": "/omdb-search/index.html"
+  },
+  {
+    "revision": "e596979d5f4a84e17ce6",
+    "url": "/omdb-search/static/css/main.b565b6c9.chunk.css"
+  },
+  {
+    "revision": "e7eee1945f099012bda0",
+    "url": "/omdb-search/static/js/2.551d886f.chunk.js"
+  },
+  {
+    "revision": "e596979d5f4a84e17ce6",
+    "url": "/omdb-search/static/js/main.01b11135.chunk.js"
+  },
+  {
+    "revision": "388f3239346918d51481",
+    "url": "/omdb-search/static/js/runtime-main.736438ec.js"
+  }
+]);
